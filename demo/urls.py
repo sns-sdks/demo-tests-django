3c6ef68c62d766ea9b2c1bd0225bc357ed7f3b12
ikaros <merle.liukun@gmail.com>
2022-12-14 12:10:56 +0800

    fix(views): fix(settings):

--- demo/urls.py
+++ demo/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import social_account_login, return_fb_login
 
 urlpatterns = [
-    path('auth/', social_account_login, name='login'),
-    path('callback/', return_fb_login, name='callback'),
+    path('auth/', social_account_login, name='social_account_login'),
+    path('callback/', return_fb_login, name='return_fb_login'),
 ]
