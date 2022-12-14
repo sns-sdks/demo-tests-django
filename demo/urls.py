acc0d6b0b57ed77af5876138f62e1a1692ae4c6b
ikaros <merle.liukun@gmail.com>
2022-12-14 12:08:01 +0800

    fix: docs: feat(initial): :sparkles: initial repo

--- demo/urls.py
+++ demo/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import social_account_login, return_fb_login
 
 urlpatterns = [
-    path(r'^auth/$', social_account_login, name='login'),
-    path(r'^callback/$', return_fb_login, name='callback'),
+    path('auth/', social_account_login, name='login'),
+    path('callback/', return_fb_login, name='callback'),
 ]
